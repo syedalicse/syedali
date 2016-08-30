48dd748ffc6b9bba0cfda7b44be915317d3dcf89
syedalicse <csesyedali@gmail.com>
2016-08-30 16:00:46 +0530

    Create cp.cpp

---
+++ public-class-DetermineLeapYearExample-public-static-void-main-String-args/year-we-want-to-check-int-year-2004/cp.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    cin >> a;
+    for (int a = 1; a <= 12; ++a) {
+        cout<<n<<" * "<<a<<" = "<<n*a<<endl;
+    }
+    return 0;
+}
