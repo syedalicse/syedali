70ebf4b03032fa4b762da7b696fbf4c3dbda1066
syedalicse <csesyedali@gmail.com>
2016-08-30 15:30:37 +0530

    Create loop.java

---
+++ public-class-DetermineLeapYearExample-public-static-void-main-String-args/year-we-want-to-check-int-year-2004/loop.java
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+ int f, c, m=0;
+ printf("Enter an integer: ");
+ scanf("%d",&f);
+ for(c=1;c<=f;++c)  
+  {
+      m+=c;               
+  }
+  printf("SUM = %d",m);
+  return 0;
+}
