4a176700b3843cd59bf56eefa0caa046696a84ae
syedalicse <csesyedali@gmail.com>
2016-08-30 15:08:11 +0530

    Rename sy.py to sy1.py

---
+++ sy1.py
@@ -0,0 +1,6 @@
+def main():     
+f = str(input('Enter a character : '))     
+if(f == 'a' or f == 'A' or f == 'e' or f == 'E' or f == 'i' or f == 'I' or f == 'o' or f == 'O' or f == 'u' or f == 'U'):         
+print('it is Vowel')     else:       
+print('Consonant') if __name__ == '__main__':    
+main()
