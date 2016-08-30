2800376a2b56625f0dab49da95d006516740fdd3
syedalicse <csesyedali@gmail.com>
2016-08-30 15:51:40 +0530

    Create ibm.java

---
+++ public-class-DetermineLeapYearExample-public-static-void-main-String-args/year-we-want-to-check-int-year-2004/ibm.java
@@ -0,0 +1,18 @@
+class primeNumber
+{
+public static void main()
+{
+int ibm,ja,ks;
+for(ibm=101;ibm<10000l;ibm++) //here you can give your own range
+{
+         ks=0;
+         for(ja=2;ja<i;ja++)
+         {
+                  if(ibm%ja==0)
+                  ks++;
+         }
+         if(ks==0)
+         System.out.print(ibm+"   ");
+}
+}
+}
