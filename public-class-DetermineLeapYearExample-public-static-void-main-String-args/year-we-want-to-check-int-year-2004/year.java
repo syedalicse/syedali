ee15cff89455956f49eb50c6a7c756d5b29d7eb3
syedalicse <csesyedali@gmail.com>
2016-08-30 15:40:24 +0530

    Create year.java

---
+++ public-class-DetermineLeapYearExample-public-static-void-main-String-args/year-we-want-to-check-int-year-2004/year.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public power
+{
+public static void main(String ar[])throws Exception
+{
+Scanner s=new Scanner(System.in);
+int n,p,a=1;
+n=s.nextInt();
+p=s.nextInt();
+for(int i=0;i<p;i++)
+{
+a*=n;
+}
+System.out.println("power of given number is:"+a);
+}
+}
