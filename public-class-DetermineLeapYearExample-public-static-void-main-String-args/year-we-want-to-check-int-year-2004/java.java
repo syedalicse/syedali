f23d5488bb9be4f418b7e1e5555dee75ea735f5a
syedalicse <csesyedali@gmail.com>
2016-08-30 15:45:22 +0530

    Create java.java

---
+++ public-class-DetermineLeapYearExample-public-static-void-main-String-args/year-we-want-to-check-int-year-2004/java.java
@@ -0,0 +1,18 @@
+public class List {
+ 
+        public static void main(String[] args) {
+               
+                
+                int l = 50;
+               
+                System.out.println("Printing Odd numbers between 1 and " + l);
+               
+                for(int j=1; j <= l; j++){
+                       
+                        
+                        if( j % 2 != 0){
+                                System.out.print(j + " ");
+                        }
+                }
+        }
+}
