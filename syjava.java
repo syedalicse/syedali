88fb05b43b9c6eacc25748912017e66f02141a5e
syedalicse <csesyedali@gmail.com>
2016-09-02 19:17:37 +0530

    Rename sy.java to syjava.java

---
+++ syjava.java
@@ -0,0 +1,11 @@
+
+class S{
+public static void main(String args[]){
+int intoInt;
+String s;
+Scanner scan=new Scanner(System.in);
+s=scan.next();
+intoInt=Integer.parseInt(s);
+System.out.println("This is an integer: "+intoInt);
+}
+}
