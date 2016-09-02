02c73a9ca56691991d778522022cbfc6ab743758
syedalicse <csesyedali@gmail.com>
2016-09-02 14:29:38 +0530

    Update pavi.c

--- pavi.c
+++ pavi.c
@@ -1,23 +1,23 @@
 #include<stdio.h>
 void main()
 {
-    int arr[1000],n,l,i,j,k,temp,flag=0,rep[1000],inc=0,check[1000];
+    int arr1[1000],n,l,i,j,k,temp,flag=0,rep[1000],inc=0,check[1000];
     scanf("%d",&n);
     for(i=0;i<n;i++)
     {
         scanf("%d",&arr[i]);
-        rep[i]=arr[i];
+        rep[i]=arr1[i];
     }
     for(i=0;i<n;i++)
     {
         flag=0;
-        temp=arr[i];
+        temp=arr1[i];
         for(j=i+1;j<n;j++)
         {
-            if(temp == arr[j])
+            if(temp == arr1[j])
             {
                flag=1;
-               arr[j]=0;
+               arr1[j]=0;
             }
         }
         if(flag == 1 && temp !=0)
