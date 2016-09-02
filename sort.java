6d0a62ca66ca53878aefa90e1d4bb17740f2d05d
syedalicse <csesyedali@gmail.com>
2016-09-02 21:47:26 +0530

    Create sort.java

---
+++ sort.java
@@ -0,0 +1,13 @@
+public class Sort implements Comparator<Integer> {
+public int compare(Integer o1, Integer o2) {
+String s1 = o1.toString();
+String s2 = o2.toString();
+return (s2+s1).compareTo(s1+s2);
+}
+public static void main(String[] args) {
+LexicographicSort ls = new LexicographicSort();
+Integer[] nums = {9,1,95,17,5};
+Arrays.sort(nums, ls);
+System.out.println(Arrays.toString(nums));
+}
+}
