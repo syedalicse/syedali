cc18fbd316dd64345c977d915f057f5e4e08ad2f
syedalicse <csesyedali@gmail.com>
2016-09-02 21:55:47 +0530

    Update dup.java

--- dup.java
+++ dup.java
@@ -3,16 +3,16 @@ public class D {
    
     public static void main(String[] args) {
         
-   String str="vishali";
+   String s="ishasli";
    char a;
    String s2="";
-   for(int i=0;i<str.length();i++)
+   for(int i=0;i<s.length();i++)
    {
        a=str.charAt(i);
        if(a!=' ')
        {
            s2=s2+a;
-           str=str.replace(a, ' ');
+           s=s.replace(a, ' ');
        }
    }
         System.out.println("the string is:"+s2);
